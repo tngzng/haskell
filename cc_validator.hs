e7ca02ef28aed94a628ca633e03617e49099565f
tngzng <stephtzhang@gmail.com>
2017-07-02 15:26:13 -0400

    cc_validator: add DobleEveryOther

--- cc_validator.hs
+++ cc_validator.hs
@@ -4,3 +4,4 @@ toDigits num = [digitToInt x | x <- show num]
 
 toDigitsRev num = [digitToInt x | x <- reverse (show num)]
 
+doubleEveryOther arr = [if y `mod` 2 == 0 then x * 2 else x| (x, y) <- zip arr [1..]]
