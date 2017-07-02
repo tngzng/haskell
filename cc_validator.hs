ecd72b898284a42e68bc18e257596ef277591b4d
tngzng <stephtzhang@gmail.com>
2017-07-02 15:48:42 -0400

    cc_validator: add validate

--- cc_validator.hs
+++ cc_validator.hs
@@ -9,3 +9,5 @@ doubleEveryOther xs = [if i `mod` 2 == 0 then x * 2 else x | (x, i) <- zip xs [1
 sumDigit x = sum (toDigits x)
 
 sumDigits xs = sum [sumDigit x | x <- xs]
+
+validate x = x `mod` 10 == 8
