f79fe29f48da27ac198afb42eba8d1a2b6d7aa1c
ting <ting.zhang@buzzfeed.com>
2017-07-03 12:30:47 -0400

    hanoi: recursive func in working order

--- hanoi.hs
+++ hanoi.hs
@@ -4,5 +4,4 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 
 hanoi 1 beg aux end = [(beg, end)]
 
--- TODO fix stack overflow err
 hanoi n beg aux end = (hanoi (n - 1) beg end aux) ++ (hanoi 1 beg aux end) ++ (hanoi (n - 1) aux beg end)
